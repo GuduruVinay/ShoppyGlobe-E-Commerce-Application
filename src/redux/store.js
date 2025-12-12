7aeaabcdd85b86c93d4c624d1c98b4298956f109
GuduruVinay <guduruvinay3@gmail.com>
2025-12-12 13:59:42 +0530

    Updated store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartSlice';
+import searchReducer from './searchSlice';
 
 export const store = configureStore({
     reducer: {
+        // Cart slice
         cart: cartReducer,
+        // Search slice
+        search: searchReducer,
     },
 });
