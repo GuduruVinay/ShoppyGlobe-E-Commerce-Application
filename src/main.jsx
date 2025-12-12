4a4747faac3b9b77431d3ff794c60223c8b7c02b
GuduruVinay <guduruvinay3@gmail.com>
2025-12-12 11:44:11 +0530

    Wrapped App with Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+// Redux imports
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
