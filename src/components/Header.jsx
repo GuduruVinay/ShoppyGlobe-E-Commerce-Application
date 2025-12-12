ec0ee5635bd0039e6be9d925dc790b041b4280ed
GuduruVinay <guduruvinay3@gmail.com>
2025-12-12 12:19:09 +0530

    Updated Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,32 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
 function Header() {
+    // Use a Redux selector to get the cart items array
+    const cartItems = useSelector((state) => state.cart.items);
+    // Calculate the total number of items
+    const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+
     return (
         <header>
             <nav>
-                <h1>ShoppyGlobe</h1>
+                {/* Link to Home */}
+                <Link to="/">
+                    <h1>ShoppyGlobe</h1>
+                </Link>
                 <ul>
-                    <li>Home</li>
-                    <li>Cart (0)</li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/cart">
+                            Cart ({totalItems})
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </header>
-    )
-}
+    );
+};
 
 export default Header;
