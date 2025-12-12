2aefab3e3e09f6bcefa24e1773c2224124962451
GuduruVinay <guduruvinay3@gmail.com>
2025-12-12 11:22:04 +0530

    Added Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
   return (
-    <h1>ShoppyGlobe E-Commerce Application</h1>
+    <div>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <nav>
+                <h1>ShoppyGlobe</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Cart (0)</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
