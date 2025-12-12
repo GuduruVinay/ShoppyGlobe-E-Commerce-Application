8a12315ef2630d06b3378dc806f7aaa99c65c4f0
GuduruVinay <guduruvinay3@gmail.com>
2025-12-12 12:28:29 +0530

    Added NotFound Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,26 @@
+import { Link, useRouteError } from "react-router-dom";
+
 function NotFound() {
+    // Use useRouteError to display specific router errors if available
+    const error = useRouteError();
+
     return (
-        <h1>NotFound</h1>
-    )
-}
+        <div>
+            <h2>404 - Page Not Found</h2>
+            <p>Sorry, the page you were looking for does not exist.</p>
+            {/* Display proper error details on the UI */}
+            {error && (
+                <div>
+                    <p>
+                        **Error Detail:** *{error.statusText || error.message}*
+                    </p>
+                </div>
+            )}
+            <p>
+                <Link to="/">Go back to the Home page</Link>
+            </p>
+        </div>
+    );
+};
 
 export default NotFound;
