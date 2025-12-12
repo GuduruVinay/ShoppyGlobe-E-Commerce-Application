84a6eb2c0e48cf1eed942bff9277f77c93d8ea9e
GuduruVinay <guduruvinay3@gmail.com>
2025-12-12 14:05:08 +0530

    Updated Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { setSearchTerm } from "../redux/searchSlice";
 
 function Header() {
+    const dispatch = useDispatch();
     // Use a Redux selector to get the cart items array
     const cartItems = useSelector((state) => state.cart.items);
+    // Get current search term
+    const searchTerm = useSelector((state) => state.search.searchTerm);
     // Calculate the total number of items
     const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+    const handleSearchChange = (event) => {
+        dispatch(setSearchTerm(event.target.value));
+    };
 
     return (
         <header>
@@ -14,6 +22,14 @@ function Header() {
                 <Link to="/">
                     <h1>ShoppyGlobe</h1>
                 </Link>
+                <div>
+                    <input 
+                        type="text"
+                        placeholder="Search products..."
+                        value={searchTerm}
+                        onChange={handleSearchChange}
+                    />
+                </div>
                 <ul>
                     <li>
                         <Link to="/">Home</Link>
